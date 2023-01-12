5b11ca3e3f16e4d566a3014af0cb510acbade75b
David Rojas Chavarria <davidmfrojas@gmail.com>
2023-01-12 00:21:16 -0600

    Se agrega el archivo de config de vite y se actualiza el docs

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: '/blackjack-vite/', //base para el path del proyecto y cargarlo en el sitio, es el nombre del repositorio de github donde se ha publicado
+});
